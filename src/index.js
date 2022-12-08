be1c0b30b545adde5a1d8f8378e16ce7268c1d56
Franco <fr3nchh111@gmail.com>
2022-12-08 03:16:07 -0300

    Commit de correcciones para el correcto funcionamiento de Vercel (otra vez)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./NavBar/Navbar.css";
-import "./NavBar/Header/Header.css";
-import "./NavBar/Footer.css";
+import "./pages/NavBar/Navbar.css";
+import "./pages/NavBar/Header/Header.css";
+import "./pages/NavBar/Footer.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import "animate.css/animate.min.css";
