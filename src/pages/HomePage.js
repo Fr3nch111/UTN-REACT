016f6d18d0352914f16ee44b9afb766137220564
Franco <fr3nchh111@gmail.com>
2022-12-13 01:26:31 -0300

    cambios en el responsive 2

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,15 +2,16 @@ import Navigation, { SocialNav } from "./NavBar/NavBar";
 import Header from "./NavBar/Header/Header";
 import { Asides } from "../Aside/Asides";
 import Footer from "./NavBar/Footer";
+import "./HomePage.css";
 
 export default function HomePage() {
   return (
     <div className="root">
       <Header className="Header"/>
-      <Navigation />
-      <SocialNav />
+      <Navigation className="Nav"/>
+      <SocialNav className="Social-nav"/>
       <Asides className="Asides" />
-      <Footer />
+      <Footer className="foot"/>
     </div>
   );
 }
